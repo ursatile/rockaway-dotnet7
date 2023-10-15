d7a288114b5e2c8b519b75156da06ede7809d744
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-15 22:59:55 +0100

    Module 402

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -11,7 +11,9 @@ Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(
+	options => options.Conventions.AuthorizeAreaFolder("admin", "/")
+);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
 
@@ -60,5 +62,10 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapGet("/status", (IStatusReporter reporter) => reporter.GetStatus());
+app.MapAreaControllerRoute(
+    name: "admin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+).RequireAuthorization();
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
