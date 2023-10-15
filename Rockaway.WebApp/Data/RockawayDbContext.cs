d8f19b46987aa1108fa99ea4dd93ed160d2b0e42
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-15 22:59:54 +0100

    Module 400

--- Rockaway.WebApp/Data/RockawayDbContext.cs
+++ Rockaway.WebApp/Data/RockawayDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Rockaway.WebApp.Data.Entities;
 using Rockaway.WebApp.Data.Sample;
 
@@ -11,10 +10,10 @@ public class RockawayDbContext : DbContext {
 	public RockawayDbContext(DbContextOptions<RockawayDbContext> options) : base(options) { }
 
 	public DbSet<Artist> Artists { get; set; } = default!;
+	public DbSet<Venue> Venues { get; set; } = default!;
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder) {
 		base.OnModelCreating(modelBuilder);
-
 		// Override EF Core's default table naming (which pluralizes entity names)
 		// and use the same names as the C# classes instead
 		foreach (var entity in modelBuilder.Model.GetEntityTypes()) {
@@ -25,6 +24,12 @@ public class RockawayDbContext : DbContext {
 			entity.HasIndex(artist => artist.Slug).IsUnique();
 		});
 
+		modelBuilder.Entity<Venue>(entity => {
+			entity.HasIndex(venue => venue.Slug).IsUnique();
+		});
+
 		modelBuilder.Entity<Artist>().HasData(SampleData.Artists.AllArtists);
+		modelBuilder.Entity<Venue>().HasData(SampleData.Venues.AllVenues);
+
 	}
 }
