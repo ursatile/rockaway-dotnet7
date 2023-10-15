069a449c5e5e70f3d32c80e3c3ea99857bae030d
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-15 22:59:54 +0100

    Module 303

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -1,32 +1,42 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Rockaway.WebApp.Data;
+using Rockaway.WebApp.Hosting;
 using Rockaway.WebApp.Services;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
 Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-Log.Information("Serilog support enabled! Yeah!");
+var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
 
-var sqliteConnection = new SqliteConnection("Data Source=:memory:");
-sqliteConnection.Open();
-builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+Log.Information("Rockaway running in {environment} environment", builder.Environment.EnvironmentName);
+if (builder.Environment.UseSqlite()) {
+	Log.Information("Using Sqlite database");
+	var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+	sqliteConnection.Open();
+	builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+} else {
+	Log.Information("Using SQL Server database");
+	var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+	builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlServer(connectionString));
+}
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment()) {
+if (app.Environment.IsProduction()) {
 	app.UseExceptionHandler("/Error");
 	app.UseHsts();
+} else {
+	app.UseDeveloperExceptionPage();
 }
 
 using (var scope = app.Services.CreateScope()) {
-	using (var db = scope.ServiceProvider.GetService<RockawayDbContext>()!) {
+	using var db = scope.ServiceProvider.GetService<RockawayDbContext>()!;
+	if (app.Environment.UseSqlite()) {
 		db.Database.EnsureCreated();
 	}
 }
@@ -39,6 +49,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.MapGet("/status", (IStatusReporter reporter) => reporter.GetStatus());
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
