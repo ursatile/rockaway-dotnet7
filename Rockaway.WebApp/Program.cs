80aa5841ff4ceca21c56ea517ed275a143cbecd4
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-15 22:59:53 +0100

    Module 202

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -1,3 +1,4 @@
+using Rockaway.WebApp.Services;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +7,10 @@ Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 Log.Information("Serilog support enabled! Yeah!");
 builder.Host.UseSerilog();
 
-// Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
 	app.UseExceptionHandler("/Error");
 	app.UseHsts();
@@ -25,5 +24,5 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.MapGet("/status", (IStatusReporter reporter) => reporter.GetStatus());
 app.Run();
