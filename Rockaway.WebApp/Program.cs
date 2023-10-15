b3eb5bb89f53314fd44c038daf97ad3b8ed2b412
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-15 22:59:53 +0100

    Module 301

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Rockaway.WebApp.Data;
 using Rockaway.WebApp.Services;
 using Serilog;
 
@@ -8,12 +11,24 @@ Log.Information("Serilog support enabled! Yeah!");
 builder.Host.UseSerilog();
 
 builder.Services.AddRazorPages();
+
 builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
+
+var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+sqliteConnection.Open();
+builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment()) {
 	app.UseExceptionHandler("/Error");
 	app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope()) {
+	using (var db = scope.ServiceProvider.GetService<RockawayDbContext>()!) {
+		db.Database.EnsureCreated();
+	}
 }
 
 app.UseHttpsRedirection();
